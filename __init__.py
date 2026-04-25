3f16b42ecc0adedf2d5848f7ec5ae4ac3cc3aa31
Atharv Aggarwal <aggarwal.atharv15@gmail.com>
2026-04-25 09:02:38 +0530

    Added init and updated gitignore

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Moving Target Environment."""
+
+from .models import MovingTargetAction, MovingTargetObservation
+
+__all__ = [
+    "MovingTargetAction",
+    "MovingTargetObservation",
+]
